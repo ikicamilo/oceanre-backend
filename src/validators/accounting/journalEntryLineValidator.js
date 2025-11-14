c8964229add7d83f6c5da32990dfacb564da78e5
ikicamilo <ikicamilo@gmail.com>
2025-11-14 18:35:02 -0500

    Zero is valid in backend

--- src/validators/accounting/journalEntryLineValidator.js
+++ src/validators/accounting/journalEntryLineValidator.js
@@ -4,7 +4,7 @@ const { body } = require('express-validator');
 exports.createJournalEntryLineValidator = [
   body('journal_entry_id').notEmpty().isInt(),
   body('account_id').notEmpty().isInt(),
-  body('debit').optional().isFloat({ gt: 0 }),
-  body('credit').optional().isFloat({ gt: 0 }),
+  body('debit').optional().isFloat({ gte: 0 }),
+  body('credit').optional().isFloat({ gte: 0 }),
   body('currency').optional().isLength({ min: 3, max: 3 }),
 ];
